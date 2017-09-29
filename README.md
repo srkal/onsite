409bed90990f3d44aa7a943c10a9c749eb9cd5cf
Pavel Srkal <srkal@quick.cz>
2017-09-29 10:18:20 +0200

    added documentation

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # onsite
 Workplace arrival/departure detector using ESP 8266 and card reader.
+## Features
+* configurable working modes / display information using menu
+* display local time and date synchronized by NTP protocol
+* display actual IP address
+* display SSID of connected network
+* read UID of RFID card and send it to Google Script
+* Google Script will translate UID to user name and store the date + time of first and last
+visit during day to Google Sheet
+* display the card owner name and duration between first and last visit
+* card cloning mode, separated to Read and Write phases
+
+## Used hardware parts
+* ESP8266 NodeMcu WIFI IoT board D1 mini with 4MB Flash
+* PCF8574P DIP-16 8-bit I/O Expander
+* RFID module RC522 Kit 13.56 Mhz
+* 16x2 Character LCD Display Module 1602/HD44780 Controller blue backlight
+* 2.54mm 2 x 40 Pin Male Double Row Pin Header Strip
+* Double side Prototype PCB Tinned Universal board 5x7 cm
+* Rotary Encoder with Push Button Switch
+* 6mm Shaft Hole Grip Knob
+* color wires
+* 2x 10KOhm resistor, 1x 5KOhm small potenciometer
+
+## Used development IDE and libraries
+Arduino IDE 1.8 or later, installed support for ESP8266 Development boards
+
+Library manager used to install build-in libraries:
+* Multiple ESP8266 libs installed with development board support (ESP8266mDNS, ...)
+* Encoder by Paul Stoffregen 1.4.1
+* NtpClientLib by German Martin 2.0.5
+* Pushbutton by Polohu 2.0.0
+* Time by Michael Margolis 1.5.0
+* Task by Makuna 1.1.4
+
+Other used libraries (directly merged to project sources)
+* [MENWIZ](https://github.com/brunialti/MENWIZ) Menu wizard for LCD displays
+* [HttpsRedirect](https://github.com/electronicsguy/ESP8266/tree/master/HTTPSRedirect) 
+Used to SSL communication with Google API
+* [LiquidCrystalI2C](https://github.com/agnunez/ESP8266-I2C-LCD1602) LCD library
+modified for ESP8266
+* [RemoteDebug](https://github.com/JoaoLopesF/ESP8266-RemoteDebug-Telnet) Provide
+debug messages over Telnet server
+
+##How it works
+Google script is published as Web Application. It should be called with single parameter
+cid containing the UID of RFID card as value:
+<pre>
+https://script.google.com/macros/s/AKfycbzd8PhytMnEhjU1GKAiXakhd8NDdnDEe2lBbhGbaVh1mNV3lQ/exec?cid=C3E602E9
+</pre>
+The script is using linked [Google sheet](https://docs.google.com/spreadsheets/d/189UJD2kwukoMbj9b04qCmCyUebviKi_xK6nt08bwLWs/edit#gid=0) as database. 
+
+First sheet "map" contains
+mapping from RFID UID to user name. For 16 character display is name limited to 10 characters.
+The sheet "template" serve as template for creating new sheets (each month one).
+
+If unknown card ID is delivered, script will create new mapping with default name
+"new-cardid".
+
+Script sends back the name of user and the duration between first and last stored 
+time as response. The response is displayed on LCD display.
+
--- onsite-8266/onsite-8266.ino
+++ onsite-8266/onsite-8266.ino
@@ -185,7 +185,6 @@ void setup() {
   
   lcd.begin(4,5);  //use dafault i2c pins SDA=4, SCL=5
   lcd.backlight();
-  lcd.home();                // At column=0, row=0
   secondRow = "STARTING";
   menu.begin(&lcd,16,2);
   initMenu();
@@ -206,8 +205,7 @@ void setup() {
  * Menu is plain - single level depth, selected mode must be confirmed by clicking the button.
  */
 void initMenu() {
-  _menu *r,*s1,*s2;
-  _var *v; 
+  _menu *r,*s1;
   r=menu.addMenu(MW_ROOT,NULL,F("Menu"));
     s1=menu.addMenu(MW_VAR,r, F("Time Date"));
       s1->addVar(MW_ACTION,myFuncTime);
@@ -218,7 +216,13 @@ void initMenu() {
     s1=menu.addMenu(MW_VAR,r, F("SSID"));
       s1->addVar(MW_ACTION,myFuncSSID);
       s1->setBehaviour(MW_ACTION_CONFIRM, false);
-
+    s1=menu.addMenu(MW_VAR,r, F("DUMP DATA"));
+      s1->addVar(MW_ACTION,myFuncDump);
+      s1->setBehaviour(MW_ACTION_CONFIRM, false);
+    s1=menu.addMenu(MW_VAR,r, F("WRITE DUMPED"));
+      s1->addVar(MW_ACTION,myFuncWrite);
+      //s1->setBehaviour(MW_ACTION_CONFIRM, false);
+    
     menu.addUsrNav(simulateNavigation,4);
     menu.addUsrScreen(userDisplay, 3000);
 }
@@ -230,14 +234,20 @@ void initMenu() {
 void userDisplay(){
   String displayText = "";
   switch (displayMode) {
+    case 0: displayText += "INACTIVE MODE\n";
+      break;
     case 1: displayText += NTP.getTimeDateString().substring(0,14) + "\n";
       break;
     case 2: displayText += WiFi.localIP().toString() + "\n";
       break;
     case 3: displayText += WiFi.SSID() + "\n";
       break;
+    case 4: displayText += "DUMP DATA\n";
+      break;
+    case 5: displayText += "WRITE DUMPED\n";
+      break;
   }
-  displayText += secondRow + "\n";
+  displayText += ((secondRow.length() > 16) ? secondRow.substring(0, 16) : secondRow) + "\n";
   if (Debug.isActive(Debug.DEBUG)) {
     Debug.println(displayText);
   }
@@ -315,6 +325,20 @@ void myFuncSSID() {
   displayMode = 3;
 }
 
+void myFuncDump() {
+  if (Debug.isActive(Debug.DEBUG)) {
+    Debug.println("MENU DUMP");
+  }
+  displayMode = 4;
+}
+
+void myFuncWrite() {
+  if (Debug.isActive(Debug.DEBUG)) {
+    Debug.println("MENU WRITE");
+  }
+  displayMode = 5;
+}
+
 /**
  * Dumps debug info about the connected PCD to display.
  */
@@ -349,6 +373,11 @@ void loop() {
   Debug.handle();
   menu.draw();
 
+  //inactive till another node selected
+  if (displayMode == 0) {
+    return;
+  }
+
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return;
@@ -358,10 +387,162 @@ void loop() {
   if ( ! mfrc522.PICC_ReadCardSerial()) {
     return;
   }
-  lastCardReaded = PCD_GetCardId(&(mfrc522.uid));
-  secondRow = "UID: " + lastCardReaded;
-  if (Debug.isActive(Debug.DEBUG)) {
-    Debug.println("UID: " + lastCardReaded);
+  if (displayMode == 4) {
+    dumpData();
+  } else {
+    if (displayMode == 5) {
+      writeData();
+    } else {
+      lastCardReaded = PCD_GetCardId(&(mfrc522.uid));
+      secondRow = "UID: " + lastCardReaded;
+      if (Debug.isActive(Debug.DEBUG)) {
+        Debug.println("UID: " + lastCardReaded);
+      }
+    }
   }
 }
 
+
+byte buffer[18];
+byte block;
+byte waarde[64][16];
+MFRC522::StatusCode status;
+    
+MFRC522::MIFARE_Key key;
+
+// Number of known default keys (hard-coded)
+// NOTE: Synchronize the NR_KNOWN_KEYS define with the defaultKeys[] array
+constexpr uint8_t NR_KNOWN_KEYS = 8;
+// Known keys, see: https://code.google.com/p/mfcuk/wiki/MifareClassicDefaultKeys
+byte knownKeys[NR_KNOWN_KEYS][MFRC522::MF_KEY_SIZE] =  {
+    {0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, // FF FF FF FF FF FF = factory default
+    {0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5}, // A0 A1 A2 A3 A4 A5
+    {0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xb5}, // B0 B1 B2 B3 B4 B5
+    {0x4d, 0x3a, 0x99, 0xc3, 0x51, 0xdd}, // 4D 3A 99 C3 51 DD
+    {0x1a, 0x98, 0x2c, 0x7e, 0x45, 0x9a}, // 1A 98 2C 7E 45 9A
+    {0xd3, 0xf7, 0xd3, 0xf7, 0xd3, 0xf7}, // D3 F7 D3 F7 D3 F7
+    {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff}, // AA BB CC DD EE FF
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00}  // 00 00 00 00 00 00
+};
+
+boolean try_key(MFRC522::MIFARE_Key *key) {
+  boolean result = false;
+    
+  for(byte block = 0; block < 64; block++){
+    status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, key, &(mfrc522.uid));
+    if (status != MFRC522::STATUS_OK) {
+      secondRow = "CRD Auth failed";
+      if (Debug.isActive(Debug.DEBUG)) {
+        Debug.println(secondRow);
+      }
+      return false;
+    }
+
+    // Read block
+    byte byteCount = sizeof(buffer);
+    status = mfrc522.MIFARE_Read(block, buffer, &byteCount);
+    if (status != MFRC522::STATUS_OK) {
+      secondRow = "CRD Read failed";
+      if (Debug.isActive(Debug.DEBUG)) {
+        Debug.println(secondRow);
+      }
+    } else {
+      // Successful read
+      result = true;
+      secondRow = "KEY OK";
+      if (Debug.isActive(Debug.DEBUG)) {
+        Debug.println("Success with key");
+      }
+      
+      Debug.println("BLOCK " + String(block));
+      for (int p = 0; p < 16; p++) {
+        waarde [block][p] = buffer[p];
+        if (Debug.isActive(Debug.DEBUG)) {
+          Debug.print(String(waarde[block][p]) + ", ");
+        }
+      }
+      Debug.println("");
+    }
+    secondRow = "DUMP finished OK";
+  }
+  
+  mfrc522.PICC_HaltA();       // Halt PICC
+  mfrc522.PCD_StopCrypto1();  // Stop encryption on PCD
+  return result;
+}
+
+/**
+ * Write the previously dumped data to another card.
+ * Used to clone card.
+ */
+void dumpData() {
+  MFRC522::MIFARE_Key key;
+  for (byte k = 0; k < NR_KNOWN_KEYS; k++) {
+    for (byte i = 0; i < MFRC522::MF_KEY_SIZE; i++) {
+        key.keyByte[i] = knownKeys[k][i];
+    }
+    if (try_key(&key)) {
+        break;
+    }
+  }
+}
+
+void writeData() { //Copy the data in the new card
+  for (byte i = 0; i < 6; i++) {
+      key.keyByte[i] = 0xFF;
+  }
+
+  for(int i = 4; i <= 62; i++){ //De blocken 4 tot 62 kopieren, behalve al deze onderstaande blocken (omdat deze de authenticatie blokken zijn)
+    if(i == 7 || i == 11 || i == 15 || i == 19 || i == 23 || i == 27 || i == 31 || i == 35 || i == 39 || i == 43 || i == 47 || i == 51 || i == 55 || i == 59){
+      i++;
+    }
+    block = i;
+    
+    status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
+    if (status != MFRC522::STATUS_OK) {
+      secondRow = "CRD Auth A failed";
+      if (Debug.isActive(Debug.DEBUG)) {
+        Debug.println(secondRow);
+      }
+      return;
+    }
+    
+    status = (MFRC522::StatusCode) mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_B, block, &key, &(mfrc522.uid));
+    if (status != MFRC522::STATUS_OK) {
+      secondRow = "CRD Auth B failed";
+      if (Debug.isActive(Debug.DEBUG)) {
+        Debug.println(secondRow);
+      }
+      return;
+    }
+
+    secondRow = "Auth OK";
+    if (Debug.isActive(Debug.DEBUG)) {
+      Debug.println(secondRow);
+    }
+    
+    status = (MFRC522::StatusCode) mfrc522.MIFARE_Write(block, waarde[block], 16);
+    if (status != MFRC522::STATUS_OK) {
+      secondRow = "Write failed";
+      if (Debug.isActive(Debug.DEBUG)) {
+        Debug.println(secondRow);
+      }
+    }
+  }
+
+  //Write UID - only some of chinese clones aloow to rewrite first block with unique id
+  if (!mfrc522.MIFARE_SetUid(waarde[0], (byte)4, true) ) {
+    secondRow = "UID Write failed";
+    if (Debug.isActive(Debug.DEBUG)) {
+      Debug.println(secondRow);
+    }
+  }
+  
+  mfrc522.PICC_HaltA();       // Halt PICC
+  mfrc522.PCD_StopCrypto1();  // Stop encryption on PCD
+  secondRow = "Write OK";
+  if (Debug.isActive(Debug.DEBUG)) {
+    Debug.println(secondRow);
+  }
+  displayMode = 0;
+}
